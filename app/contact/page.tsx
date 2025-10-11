0708bfa36767818dfa5c6a801c08c8d8788b0c23
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-10-11 14:19:17 -0500

    client and server components

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,13 @@
-export default function Contact() {
+import ButtonComponent from "./button";
+
+export default async function Contact() {
+  console.log("Is this in server or client?");
+
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+  console.log(posts);
+
   return (
-    <p>Contact Page</p>
+    <ButtonComponent />
   )
 }
