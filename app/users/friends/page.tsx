32f0b6e68669e2081c8875ec1cad1137b9eeda8d
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-10-13 22:43:23 -0500

    API routes

--- app/users/friends/page.tsx
+++ app/users/friends/page.tsx
@@ -1,3 +1,18 @@
-export default function Friends() {
-  return <h1>Friends List</h1>;
+async function makePostRequest() {
+  const res = await fetch(`${process.env.NEXT_URL}/api/hello`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name: "John Doe" }),
+  });
+
+  const data = await res.json();
+  return { data };
+}
+
+export default async function Friends() {
+  const { data } = await makePostRequest();
+
+  return <h1>Heyy Friends, {data.message}</h1>;
 }
